09866cd2b592004d3ec20a06aa7eba2b3d6954e2
Lewis <dukelowlewis@gmail.com>
2021-02-21 21:21:20 +0000

    Added logging

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -7,6 +7,7 @@ const stripe = require('stripe')(process.env.STRIPE_KEY);
 const router = express.Router();
 const dao = require('../dao/dataOrder.js');
 const authorisation = require('../middleware/auth.js');
+const logger = require('../middleware/logger.js');
 
 router.post('/create-payment-intent', authorisation.isAuthorized, async (req, res, next) => {
   const { orderID } = req.body;
@@ -29,6 +30,7 @@ router.post('/create-payment-intent', authorisation.isAuthorized, async (req, re
       currency: 'gbp',
       metadata: { order_id: orderID },
     });
+    logger.info('Payent intent created', { orderID, userID: res.locals.user, paymentIntent: paymentIntent.id, orderTotal: total, OrderFee: fee });
 
     // update price
     try {
@@ -39,6 +41,7 @@ router.post('/create-payment-intent', authorisation.isAuthorized, async (req, re
         orderID,
         res.locals.user,
       );
+      logger.info('Added order price to database', { orderID, userID: res.locals.user, orderTotal: total, OrderFee: fee });
       console.log(addPricetoOrder);
     } catch (error) {
       next(error);
@@ -47,6 +50,7 @@ router.post('/create-payment-intent', authorisation.isAuthorized, async (req, re
     res.send({
       clientSecret: paymentIntent.client_secret,
     });
+    logger.info('Returned client secrt for payment intent', { orderID, userID: res.locals.user, paymentIntent: paymentIntent.id });
   } catch (error) {
     next(error);
   }
@@ -61,12 +65,13 @@ router.get('/intent/:orderID', authorisation.isAuthorized, async (req, res, next
     if (paymentIntentID[0].payment_id) {
       try {
         const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentID[0].payment_id);
-        // console.log(paymentIntent);
+        logger.info('Got payment intent for order', { orderID, userID: res.locals.user, paymentIntent: paymentIntent.id });
         res.json({ clientSecret: paymentIntent.client_secret });
       } catch (error) {
         next(error);
       }
     } else {
+      logger.info('No payment intent found for order', { orderID, userID: res.locals.user });
       res.status(404).send();
     }
   } catch (error) {
@@ -87,10 +92,12 @@ async function handlePaymentIntentSucceeded(id) {
 }
 
 router.post('/webhook', async (req, res, next) => {
+  logger.info('Stripe webhook event recived', { request: req.body });
   let event;
   try {
     event = req.body;
   } catch (err) {
+    logger.error('Stripe webhook error while parsing request.', { error:  err.message });
     console.log('⚠️  Webhook error while parsing basic request.', err.message);
     return res.send();
   }
@@ -98,21 +105,21 @@ router.post('/webhook', async (req, res, next) => {
   switch (event.type) {
     case 'payment_intent.succeeded':
       const intent = event.data.object;
+      logger.info('Stripe webhook event parsed', { event: 'payment_intent.succeeded', paymentIntentID: intent.id });
       console.log('Succeeded:', intent.id);
       const response = await handlePaymentIntentSucceeded(intent.id);
+      logger.info('Order status updated', { event: 'payment_intent.succeeded', paymentIntentID: intent.id });
       if (response !== true) {
         next(response);
       }
-
       // send conformation email
         // get email address from order ID
-
       break;
     default:
       // Unexpected event type
       console.log(`Unhandled event type ${event.type}.`);
+      logger.info('Stripe webhook warning', { event: `${event.type}`, eventID: event.id });
   }
-  // Return a 200 response to acknowledge receipt of the event
   res.send();
 });
 
