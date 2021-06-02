20050c8a4a86153222af9b5726d5778062687010
Lewis <dukelowlewis@gmail.com>
2021-06-02 22:35:37 +0100

    Added listing orders in ops tooling
    - Added order picking completing functionality
    - Updated google client key

--- tools/inverops/main.js
+++ tools/inverops/main.js
@@ -1,79 +1,61 @@
 const API_URL = "http://localhost:3002";
 
-const orderList = document.querySelector(".orderList");
+const ordersList = document.querySelector(".ordersList");
 
-const url_string = window.location.href;
-const url = new URL(url_string);
-const orderID = url.searchParams.get("orderID");
-console.log(orderID);
+getOrders();
 
-
-getOrderContent();
-
-
-
-async function getOrderContent() {
+async function getOrders() {
     try {
-        const response = await fetch(`${API_URL}/order/${orderID}`);
-        const orderItems = await response.json();
-        console.log(orderItems);
-        orderItems.forEach(orderItem => {
-            displayOrder(orderItem);
+        const response = await fetch(`${API_URL}/order/all`);
+        const orders = await response.json();
+        console.log(orders);
+        orders.forEach(order => {
+            displayOrder(order);
         });
     } catch (error) {
         console.error(error);
     } 
 }
 
-function displayOrder(product) {
-    const orderDiv = document.createElement("div");
-    orderDiv.setAttribute("class", "orderItem");
+function displayOrder(order) {
 
-    const imageAndNameDiv = document.createElement("div");
-    imageAndNameDiv.setAttribute("class", "orderImageAndName");
-
-    const img = document.createElement("img");
-    img.setAttribute("src", product.image_url);
-    img.setAttribute("width", "75px");;
-
-    const productName = document.createElement("p");
-    productName.innerHTML = `(${product.quantity}x) ${product.name}`;
-
-    imageAndNameDiv.appendChild(img);
-    imageAndNameDiv.appendChild(productName);
-
-    const buttonGroupDiv = document.createElement("div");
-    buttonGroupDiv.setAttribute("class", "buttonGroup");
-
-    const noButton = document.createElement("button");
-    noButton.setAttribute("id", "noItemButton");
-    noButton.innerHTML = "Can't find";
-    noButton.addEventListener("click", () => {
-        console.log(product.product_id);
+    const link = document.createElement("a");
+    link.setAttribute("href", "javascript:;");
+    link.setAttribute("class", "order");
+    link.addEventListener("click", () => {
+        console.log("click");
+        window.location.href = `./order/?orderID=${order.order_id}`
     })
 
-    const yesButton = document.createElement("button");
-    yesButton.setAttribute("id", "yesnoItemButton");
-    yesButton.innerHTML = "Found";
-    yesButton.addEventListener("click", () => {
-        console.log(product.product_id);
-        console.log(typeof(orderDiv.style.opacity));
-        if (orderDiv.style.opacity === "0.4") {
-            orderDiv.setAttribute("style","opacity:1.0; -moz-opacity:1.0");
-            yesButton.innerHTML = "Found";
-        } else {
-            orderDiv.setAttribute("style","opacity:0.4; -moz-opacity:0.4");
-            yesButton.innerHTML = "Unfound";
-        }
-        
-    })
+    const div = document.createElement("div");
+    div.setAttribute("class", "orderText");
+
+    const orderID = document.createElement("p");
+    orderID.innerHTML = order.order_id;
+
+    const userID = document.createElement("p");
+    userID.innerHTML = order.user_id;
+
+    const deliveryDate = new Date(order.time);
+    const displaydate = deliveryDate.toLocaleDateString("en-GB", {
+        year: '2-digit',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: 'numeric',
+        hour12: true,
+    });
+
+    const time = document.createElement("p");
+    time.innerHTML = displaydate;
+
+    div.appendChild(orderID);
+    div.appendChild(userID);
+    div.appendChild(time);
+
+    link.appendChild(div);
+
+    ordersList.appendChild(link);
 
 
-    buttonGroupDiv.appendChild(noButton);
-    buttonGroupDiv.appendChild(yesButton);
-
-    orderDiv.appendChild(imageAndNameDiv);
-    orderDiv.appendChild(buttonGroupDiv);
-
-    orderList.appendChild(orderDiv);
 }
