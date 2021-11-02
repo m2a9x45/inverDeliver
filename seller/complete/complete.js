dd7697c9b52c9945952b83a234422d3ae94ca730
Lewis <dukelowlewis@gmail.com>
2021-11-02 20:15:05 +0000

    Url changes

--- seller/complete/complete.js
+++ seller/complete/complete.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3001';
+const API_URL = 'https://api.inverdeliver.com';
 
 console.log(localStorage.getItem("sellerToken")); 
 
--- seller/login/login.js
+++ seller/login/login.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3001'
+const API_URL = 'https://api.inverdeliver.com'
 const signupForm = document.querySelector('#signupForm');
 
 signupForm.addEventListener('submit', (e) => {
