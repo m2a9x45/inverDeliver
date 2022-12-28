4919589fff03757733a9851a7b790a47c2f17815
Lewis <dukelowlewis@gmail.com>
2022-12-28 23:35:00 +0000

    urls

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -21,7 +21,7 @@ const loginToken = new URL(window.location.href).searchParams.get("token");
 
 if (loginToken) {
   localStorage.setItem('token', loginToken);
-  window.location = '../index.html';
+  window.location = 'https://inverdeliver.com/';
 }
 
 // Navbar toggle code
